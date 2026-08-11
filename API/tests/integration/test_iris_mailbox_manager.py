ab799f001ebe0ca27a5fe7b0fc37d9bfcc77cc4e
Gabriel Musteata <gmiganescu@gmail.com>
2026-08-11 13:42:11 +0200

    fix: failure test in github environment

--- API/tests/integration/test_iris_mailbox_manager.py
+++ API/tests/integration/test_iris_mailbox_manager.py
@@ -19,6 +19,7 @@ from src.modules.features.iris.exceptions import (
     IrisMailboxOAuthStateError,
     IrisMailboxQuotaExceededError,
 )
+import src.modules.features.iris.managers.analysis as analysis_managers_mod
 from src.modules.features.iris.managers.mailbox import IrisMailboxManager
 from src.modules.features.iris.model import IrisAnalysis, IrisMailboxConnection
 from src.modules.features.iris.repositories import IrisAnalysisRepository, IrisMailboxConnectionRepository
@@ -325,7 +326,9 @@ def test_sync_connection_ingests_new_messages_and_advances_cursor(app, regular_u
     with app.app_context():
         connection_id = _save(app, _connection(regular_user.id, sync_cursor="cursor-0"))
 
-        with mock.patch.object(mailbox_managers_mod, "get_connector", return_value=_FakeConnector()):
+        fake_queue = _FakeTaskQueue()
+        with mock.patch.object(mailbox_managers_mod, "get_connector", return_value=_FakeConnector()), \
+             mock.patch.object(analysis_managers_mod.TaskQueue, "get_instance", return_value=fake_queue):
             IrisMailboxManager()._sync_connection(connection_id)
 
         with UnitOfWork() as uow:
