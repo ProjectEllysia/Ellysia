47620fb6b5d7cdf1c6555b15a9bcbc655e029653
Gabriel Musteata <gmiganescu@gmail.com>
2026-08-28 10:55:00 +0200

    chore(db): reencadenar la migración de required_os tras el merge de cabezas
    4fe4383a5085 dependía directamente de a1b2c3d4e5f7, una de las dos
    cabezas de Alembic sin fusionar que ya existían en develope. Ahora
    depende de baf6f0e7fd8c (el merge, cherry-pickeado de
    feat/aegis/white-labeling), dejando una única cabeza en la rama.
    
    Se reutiliza el mismo commit/ID de merge en vez de crear uno propio
    para que, fusione lo que fusione primero (esta rama o
    feat/aegis/white-labeling), no se produzca un segundo punto de fusión
    duplicado en develope.

--- API/alembic/versions/4fe4383a5085_cpe_match_and_finding_required_os.py
+++ API/alembic/versions/4fe4383a5085_cpe_match_and_finding_required_os.py
@@ -1,7 +1,7 @@
 """cpe match and finding required_os
 
 Revision ID: 4fe4383a5085
-Revises: a1b2c3d4e5f7
+Revises: baf6f0e7fd8c
 Create Date: 2026-08-28 00:00:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '4fe4383a5085'
-down_revision: Union[str, Sequence[str], None] = 'a1b2c3d4e5f7'
+down_revision: Union[str, Sequence[str], None] = 'baf6f0e7fd8c'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
